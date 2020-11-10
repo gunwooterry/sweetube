065804aec44163328bca63aaec9783bb33fe9d87
victory-jooyon <gina611.kaist@gmail.com>
2020-11-10 21:05:10 +0900

    Add fine chunk using punctuation prediction

--- sweetube/chunk/base.py
+++ sweetube/chunk/base.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from typing import List
+import requests
+from tqdm import tqdm
 
 
 @dataclass
@@ -23,6 +25,25 @@ class TranscriptData:
 
     def split(self, sep='.'):
         texts = self.text.split(sep)
+        cur = 0
+        res = []
+        for t in texts:
+            duration = self.duration * len(t) / len(self.text)
+            res.append(self.__class__(
+                t.strip(),
+                self.start + cur,
+                duration,
+            ))
+            cur += duration
+        return res
+
+    def punctuate(self):
+        self.text = self.text.replace('\n', ' ')
+        data = {
+            'text': self.text,
+        }
+        res = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
+        texts = res.content.decode().split('.')
         cur = 0
         res = []
         for t in texts:
@@ -69,3 +90,21 @@ class SimpleChunk(ChunkABC):
                 exceeded = self.threshold > 0 and sum(len(d.text.split(' ')) for d in partitioned_data[-1]) > self.threshold
         return [TranscriptData.combine(arr) for arr in partitioned_data]
 
+
+class FineChunk(SimpleChunk):
+    def __init__(self, threshold_words=15):
+        super().__init__(threshold_words)
+
+    def _chunk(self, data: List[TranscriptData]) -> List[TranscriptData]:
+        result = super()._chunk(data)
+        splitted_data = [e for d in tqdm(result) for e in d.punctuate()]
+
+        self.threshold = 3
+        partitioned_data = []
+
+        for d in splitted_data:
+            if len(d.text.split(' ')) > self.threshold:
+                partitioned_data.append([d])
+            else:  # 너무 짧은 경우만 앞이랑 이어주기
+                partitioned_data[-1].append(d)
+        return [TranscriptData.combine(arr) for arr in partitioned_data]
