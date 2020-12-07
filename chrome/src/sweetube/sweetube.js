9738ea5a2d2ca6cb8063314451d7bf9e8559a170
Seongwon Lee <spacebarley@Seongwonui-MacBookPro.local>
2020-12-08 03:33:06 +0900

    Add basic functions for modal

--- chrome/src/sweetube/sweetube.js
+++ chrome/src/sweetube/sweetube.js
@@ -12,10 +12,18 @@ import { KIND as ButtonKind } from 'baseui/button';
 
 function getTimestamp(second) {
   let rounded = Math.round(second);
-  return `${Math.floor(rounded / 60)}:${rounded % 60}`;
+  let min = Math.floor(rounded / 60)
+  let sec = rounded % 60
+  min = 10 > min ? '0' + min : min;
+  sec = 10 > sec ? '0' + sec : sec;
+  return `${min}:${sec}`;
 }
 
 export default function Sweetube({ data, isOpen, onClose }) {
+  function backToPreviousPage() {
+    window.history.back();
+    onClose();
+  }
   return (
     <Modal
       onClose={onClose}
@@ -24,16 +32,23 @@ export default function Sweetube({ data, isOpen, onClose }) {
       animate
       size={SIZE.auto}
       role={ROLE.dialog}
+      overrides={{
+        Root: {
+          style: () => ({
+            zIndex: '9999',
+          })
+        }
+      }}
     >
       <ModalHeader>Hate Speech Detected</ModalHeader>
       <ModalBody>
         {data.map(s => <p>{s.text} (at {getTimestamp(s.start_time)})</p>)}
       </ModalBody>
       <ModalFooter>
-        <ModalButton kind={ButtonKind.tertiary}>
+        <ModalButton kind={ButtonKind.tertiary} onClick={onClose}>
           Skip
         </ModalButton>
-        <ModalButton>Stop Watching</ModalButton>
+        <ModalButton onClick={backToPreviousPage}>Stop Watching</ModalButton>
       </ModalFooter>
     </Modal>
   )
