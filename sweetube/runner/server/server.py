cedc0078aa43f41822cff8b7a49b21d5fcd8f668
Hang Park <hangpark@kaist.ac.kr>
2020-12-11 20:25:00 +0900

    Add feedback API

--- sweetube/runner/server/server.py
+++ sweetube/runner/server/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from datetime import datetime
+
+from flask import Flask, request, jsonify
 
 from sweetube.runner import run
 from sweetube.transcript import TranscriptorFailed
@@ -14,5 +16,16 @@ def calculate(video_id):
     try:
         result = run(video_id, only_manually, only_hate, get_csv)
     except TranscriptorFailed as e:
-        return {"message": e.msg}, 400
-    return {"data": result}
+        return {'message': e.msg}, 400
+    return jsonify({'data': result})
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    from pprint import pprint
+    video_id = request.json['video_id']
+    is_valid = request.json['is_valid']
+    phrase = request.json['phrase']
+    now = datetime.now().isoformat()
+    with open('./feedback.csv', 'a+') as f:
+        f.write(f'{now},{video_id},{is_valid},{phrase}\n')
+    return jsonify({"status": "ok"})
